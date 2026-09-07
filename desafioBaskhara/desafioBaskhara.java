ff731c012f44fe1551d764408dd1d5348d6a06ed
Gus <101070070+Gust4v1n@users.noreply.github.com>
2026-09-06 23:03:10 -0300

    resolucao de baskhara

---
+++ desafioBaskhara/desafioBaskhara.java
@@ -0,0 +1,24 @@
+package desafioBaskhara;
+
+import java.lang.Math;
+/*Leia 3 valores de ponto flutuante e efetue o cálculo das raízes da equação de Bhaskara. Se não for possível calcular as raízes, mostre a mensagem correspondente “Impossivel calcular”, caso haja uma divisão por 0 ou raiz de numero negativo.
+
+Entrada
+Leia três valores de ponto flutuante (double) A, B e C.
+
+Saída
+Se não houver possibilidade de calcular as raízes, apresente a mensagem "Impossivel calcular". Caso contrário, imprima o resultado das raízes com 5 dígitos após o ponto, com uma mensagem correspondente conforme exemplo abaixo. Imprima sempre o final de linha após cada mensagem.
+*/
+import java.util.Scanner;
+public class desafioBaskhara {
+    public static void main(String[] args) {
+        double a = 10.0;
+        double b = 20.1;
+        double c = 5.1;
+        double delta = (b * b) - 4 * (a * c);
+        double baskharaPositivo = (-b + Math.sqrt(delta)) / (2 * a);
+        double baskharaNegativo = (-b - Math.sqrt(delta)) / (2 * a);
+        System.out.printf("R1 = %.5f\n", baskharaPositivo);
+        System.out.printf("R2 = %.5f", baskharaNegativo);
+    }
+}
